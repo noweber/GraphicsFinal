a0a77f0d54dfd9d7b24a51ba664dfe4c3d4061f0
noweber <noweber@gmail.com>
2014-11-22 15:14:38 -0600

    Adding Code::Blocks starter files

---
+++ Project/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// This is a very complex Hello World program.
+// Do not attempt to master it all in one sitting.
+int main() {
+    std::cout << "Hello, GitHub.\n";
+    return 0;
+}
