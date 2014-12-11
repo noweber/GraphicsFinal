75313d52d01d5acf49c9d036153008947c37e3d9
Hongbin Zhang <zhan2385@umn.edu>
2014-12-11 06:45:52 -0600

    modelIndex and some model file added

---
+++ Project/modelIndex.cpp
@@ -0,0 +1,11 @@
+#include "modelIndex.h"
+
+modelIndex::modelIndex(int _start, int _end)
+{
+        start = _start;
+        end = _end;
+}
+
+modelIndex::modelIndex() {
+
+}
---
+++ Project/modelIndex.h
@@ -0,0 +1,8 @@
+class modelIndex {
+public:
+    int start, end;
+
+public:
+	modelIndex();
+    modelIndex(int _start, int _end);
+};
