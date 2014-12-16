adddce1f05ca5739c975bd2f80933edda758ee42
noweber <noweber@gmail.com>
2014-12-15 20:16:58 -0600

    Working Collision Detection

--- Project/Level.cpp
+++ Project/Level.cpp
@@ -43,6 +43,9 @@ Level::Level(int laneWidth, int numberOfLanes) {
 
     hasFailed = false;
     hitPoints = 2;
+
+    playerPosXOnLevel = 0;
+    passedLaneOne = false;
 }
 
 void Level::update(float dt) {
@@ -52,11 +55,67 @@ void Level::update(float dt) {
 
     // Check if the front lane needs to be swapped and a collision check needs to occur
     if(dzSinceSwap > laneSpacing) {
-        frontLane += 1;
-        mustCheckCollisions = true;
+        if(passedLaneOne) {
+            frontLane += 1;
+            mustCheckCollisions = true;
+        } else {
+            passedLaneOne = true;
+        }
+
+        if(frontLane >= nLanes) {
+            hasFailed = true;   /// TODO-- make them win
+        }
         dzSinceSwap -= laneSpacing;
         // Increase the lane velocity
-        zVelocity += 0.00001;
+        zVelocity += 0.00005;
+    }
+
+    // Check for collisions
+    if(mustCheckCollisions) {
+        bool didCollide = true;
+        // Trying to use integer
+        //for(int i = 0; i < lanes[frontLane].nPaths; i++) {
+        if(playerPosXOnLevel < 0 || playerPosXOnLevel >= lanes[frontLane].nPaths) {
+            //hasFailed = true;
+        } else {
+            if(lanes[frontLane].paths[playerPosXOnLevel] == 0) {
+                didCollide = false;
+            }
+        }
+
+        // Using floats...
+        //int collisionForgiveness = 0.5f;
+        int check1 = ceil(playerFloatX);
+        int check2 = float(playerFloatX);
+        std::cout << "Check 1: " << check2 << "\n";
+        std::cout << "Check 2: " << check2 << "\n";
+
+        if(check1 >= 0 && check1 <= lWidth) {
+            if(lanes[frontLane].paths[check1] == 0) {
+                std::cout << "Didn't collide...\n";
+                didCollide = false;
+            }
+        }
+
+        if(check2 >= 0 && check2 <= lWidth) {
+            if(lanes[frontLane].paths[check2] == 0) {
+                std::cout << "Didn't collide...\n";
+                didCollide = false;
+            }
+        }
+
+        if(didCollide) {
+                std::cout << "Player Position On Level: " << playerPosXOnLevel << "\n";
+                std::cout << "Object At Path Position: " << lanes[frontLane].paths[playerPosXOnLevel] << "\n";
+                std::cout << "Current Lane: ";
+                for(int j = 0; j < lanes[frontLane].nPaths; j++) {
+                    std::cout << lanes[frontLane].paths[j];
+                }
+                std::cout << "\n";
+                hasFailed = true;
+        }
+
+        mustCheckCollisions = false;
     }
 
 }
